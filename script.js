405e99e0e112a66b9eb3b38d4cbc7e6cacbad8b9
madawaMax <madawavishwajith7@gmail.com>
2024-08-11 16:18:34 +0530

    hari

---
+++ script.js
@@ -0,0 +1,246 @@
+function key(event) {
+
+    if (event.which == 13) {
+
+        if (rw == 0) {
+
+            fid = f();
+            rw = setInterval(run, 100);
+            rs.play();
+
+            bw = setInterval(ba, 100);
+            sw = setInterval(updateScore, 100);
+            fw = setInterval(move, 100);
+        }
+    }
+
+    if (event.which == 32) {
+       
+        if (jw == 0) {
+
+            clearInterval(rw);
+            rs.pause();
+
+            jw = setInterval(jump, 100);
+            js.play();
+        }
+    }
+}
+
+
+var fid = 0;
+var p = 800;
+
+function f() {
+
+    for (var y = 0; y < 10; y++) {
+        var i = document.createElement("img");
+        i.src = "flame.gif";
+        i.className = "f";
+        i.style.marginLeft = p + "px";
+
+        if (y <= 5) {
+            p = p + 600;
+        }
+        if (y >= 6) {
+            p = p + 400;
+        }
+
+        i.id = "d" + y;
+
+        document.getElementById("b").appendChild(i);
+    }
+}
+
+var rw = 0;
+var r = 1;
+
+function run() {
+
+    var rimg = document.getElementById("boy");
+    r = r + 1;
+    if (r == 9) {
+        r = 1;
+    }
+
+    rimg.src = "Run ("+ r +").png";
+}
+
+
+
+
+
+
+
+var bw = 0;
+var u = 0;
+
+function ba() {
+    u = u - 20;
+    document.getElementById("b").style.backgroundPositionX = u + "px";
+}
+
+
+
+var sw = 0;
+var a = 0;
+
+function updateScore() {
+    a = a + 5;
+    document.getElementById("score").innerHTML = a;
+
+    if (a == 650) {
+        level();
+    }
+}
+
+function level() {
+    document.getElementById("level").style.visibility = "visible";
+    document.getElementById("endscore").innerHTML = a;
+
+    ls.play();
+
+
+    clearInterval(rw);
+                rs.pause();
+
+                clearInterval(jw);
+                j = -1;
+                clearInterval(sw);
+                clearInterval(fw);
+                clearInterval(bw);
+                clearInterval(dw);
+                ds.pause();
+}
+
+
+
+var fw = 0;
+
+function move() {
+
+    for (var y = 0; y < 10; y++) {
+        var z = getComputedStyle(document.getElementById("d" + y));
+        var p = parseInt(z.marginLeft) - 20;
+
+        document.getElementById("d" + y).style.marginLeft = p + "px";
+
+
+        // 20   140
+        if (p >= 20 & p <= 90) {
+            
+            if (mt > 290) {
+                
+                
+                clearInterval(rw);
+                rs.pause();
+
+                clearInterval(jw);
+                j = -1;
+                clearInterval(sw);
+                clearInterval(fw);
+                clearInterval(bw);
+
+                dw = setInterval(dead, 100);
+                ds.play();
+            }
+        }
+    }
+}
+
+
+
+
+var jw = 0;
+var j = 1;
+var mt = 370; // boy margintop in css
+
+function jump() {
+    var jimg = document.getElementById("boy");
+
+    if (j <= 6) { // 1 - 6 images
+        mt = mt - 40;
+    }
+    if (j >= 7) {
+        mt = mt + 40;
+    }
+    jimg.style.marginTop = mt + "px";
+
+
+    j = j + 1;
+
+    if (j == 13) {
+        j = 1;
+
+        clearInterval(jw);
+        rw = setInterval(run, 100);
+        rs.play();
+        
+        jw = 0;
+
+        if (fid == 0) {
+            fid = f();
+        }
+        if (bw == 0) {
+            bw = setInterval(ba, 100);
+        }
+        if (sw == 0) {
+            sw = setInterval(updateScore, 100);
+        }
+        if (fw == 0) {
+            fw = setInterval(move, 100);
+        }
+    }
+    jimg.src = "Jump (" + j + ").png";
+}
+
+
+
+
+
+var dw = 0;
+var d = 0;
+
+function dead() {
+ 
+    var dimg = document.getElementById("boy");
+
+    d = d + 1;
+
+    if (d == 11) {
+        d = 10;
+
+        dimg.style.marginTop = "370px";
+
+        document.getElementById("end").style.visibility = "visible";
+        document.getElementById("endscore").innerHTML = a;
+
+    }
+    dimg.src = "Dead (" + d + ").png";
+}
+
+
+
+
+function re() {
+    location.reload();
+
+}
+
+
+
+
+
+
+
+
+
+
+var rs = new Audio("walk.wav");
+var js = new Audio("jump.mp3");
+var ds = new Audio("dead.mp3");
+var ls = new Audio("level2.wav");
+
+
+
+
